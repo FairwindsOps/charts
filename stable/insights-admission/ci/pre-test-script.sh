6a6b0a79de1af93ed90573d52244554bb6a37b81
Bader Boland <bader@fairwinds.com>
2020-09-23 10:31:01 -0400

    Use cert-manager

---
+++ stable/insights-admission/ci/pre-test-script.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env sh
+kubectl create namespace cert-manager
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --version v1.0.1 \
+  --set installCRDs=true
