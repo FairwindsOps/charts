731170aed62f7933b2a323723b9dbb8fc78871b3
Bader Boland <bader@fairwinds.com>
2020-12-15 16:18:26 -0500

    Fix NS

--- stable/insights-agent/ci/pre-test-script.sh
+++ stable/insights-agent/ci/pre-test-script.sh
@@ -2,7 +2,7 @@
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 kubectl create namespace cert-manager || echo "Namespace already exists"
-helm get notes cert-manager || helm install \
+helm get notes -n cert-manager cert-manager || helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --version v1.0.1 \
