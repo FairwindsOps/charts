2c0e957dac93eb47a637df5a3f08832a7a451af3
Robert Brennan <contact@rbren.io>
2022-02-04 10:02:54 -0500

    [insights-agent] Fix version parsing (#694)
    * Update download-kubectl.sh
    
    * Update Chart.yaml
    
    * Update Chart.yaml
    
    * disable falco
    
    * better pipefail
    
    * fix up download script
    
    * fix script
    
    * fix script
    
    * update falco
    
    * no wait for falco
    
    * Update download-kubectl.sh

--- stable/insights-agent/download-kubectl.sh
+++ stable/insights-agent/download-kubectl.sh
@@ -1,29 +1,41 @@
 #! /bin/sh
 
-set -e
+set -eo pipefail
 
 mkdir /tmp/bin
 export PATH=$PATH:/tmp/bin
 cd /tmp/bin
+
 # Download kubectl to match the cluster version,
 # using kubectl 1.19 for clusters <= 1.19.
-default_kubectl_version='v1.19.6'
+
+old_kubectl_version='v1.19.6'
+default_kubectl_version=$(curl -fsSL https://dl.k8s.io/release/stable.txt)
 echo "Downloading jq . . ."
-curl -sSLo jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
+curl -fsSLo jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
+
 echo "Getting the Kubernetes version from the API. . ."
-kube_version=$(curl -ksS https://kubernetes.default.svc/version?timeout=32s | jq -r .gitVersion | cut -d- -f1)
-echo "Found Kubernetes version $kube_version"
-kube_minor_version=$(echo $kube_version |cut -d. -f2)
+kube_version_info=$(curl -kfsSL https://kubernetes.default.svc/version?timeout=32s)
+echo "Found Kubernetes version info: $kube_version_info"
+git_version=$(echo "${kube_version_info}" | jq -r .gitVersion | cut -d- -f1)
+echo "Git version is ${git_version}"
+kube_minor_version=$(echo "${kube_version_info}" | jq -r .minor)
 echo "Minor version is ${kube_minor_version}"
-if [ "x${kube_version}" == "x" ] ; then
-  kubectl_version="${default_kubectl_version}"
-  echo "Unable to find cluster version. Using kubectl version ${kubectl_version}"
-elif [ "$kube_minor_version" -gt 19 ] ; then
-  kubectl_version="${kube_version}"
-  echo "Using kubectl version ${kubectl_version} to match the cluster"
-else
-  kubectl_version="${default_kubectl_version}"
-  echo "Using kubectl version ${kubectl_version} because the cluster is <= version 1.19"
+
+
+version_pattern='v[0-9]+\.[0-9]+.[0-9]+)'
+kubectl_version=""
+if echo "${git_version}" | grep "^v[0-9]\+\.[0-9]\+\.[0-9]\+$"; then
+  kubectl_version="${git_version}"
 fi
+if [ "$kube_minor_version" -lt 19 ] ; then
+  echo "Cluster version earlier than 1.19. Using kubectl version ${old_kubectl_version}"
+  kubectl_version="${old_kubectl_version}"
+fi
+if [ "${kubectl_version}" == "" ] ; then
+  echo "Unable to find cluster version. Using kubectl version ${default_kubectl_version}"
+  kubectl_version="${default_kubectl_version}"
+fi
+
 echo Downloading kubectl version ${kubectl_version}
-curl -sSLo kubectl "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl" && chmod +x kubectl
+curl -fsSLo kubectl "https://dl.k8s.io/release/${kubectl_version}/bin/linux/amd64/kubectl" && chmod +x kubectl
