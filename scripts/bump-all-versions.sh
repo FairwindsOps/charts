e260d421bf20726b934f79c97dc3591af08225db
Robert Brennan <contact@rbren.io>
2023-02-08 13:46:35 -0500

    update script

--- scripts/bump-all-versions.sh
+++ scripts/bump-all-versions.sh
@@ -3,15 +3,12 @@ set -eo pipefail
 
 folders=( ./stable ./incubator )
 for d in "${folders[@]}"; do
-  for chart in $d/* ; do
-      echo "$chart"
-      if [ ! -d $chart ]; then
+  for chart in "${d}/*" ; do
+      if [ ! -d "${chart}" ]; then
         continue
       fi
-      version=$(cat $chart/Chart.yaml | yq eval '.version' -)
-      echo "current version:" $version
-      version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-      echo "new version: " $version
+      version=$(cat "$chart/Chart.yaml" | yq eval '.version' -)
+      version=$(echo "$version" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
       yq eval -i ".version = \"$version\"" "$chart/Chart.yaml"
   done
 done
