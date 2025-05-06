c9a900d2a2e4d1d9aa8cd83045674dea89fbb55b
Mad Mirrajabi <mohammadmirrajabi@gmail.com>
2025-05-06 22:17:17 +0200

    chore: fix a typo in documentation

--- stable/goldilocks/ci/pre-test-script.sh
+++ stable/goldilocks/ci/pre-test-script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This will label the first ndoe that is returned by kubectl get nodes so that we can test
-# the topologySpreadConstraints with `topologyKey: topology.kubernetes.io/zone`
+# This will label the first node that is returned by kubectl get nodes so that we can test
+# the `topologySpreadConstraints` with `topologyKey: topology.kubernetes.io/zone`
 FIRST_NODE="$(kubectl get nodes -o name | head -n 1)"
 kubectl label "$FIRST_NODE" topology.kubernetes.io/zone=test-zone-a
