ac16d4b3b66fae5d382322ede123315e2592e0ef
Mad Mirrajabi <mohammadmirrajabi@gmail.com>
2025-05-06 22:06:05 +0200

    chore: fix bash lint issues

--- stable/goldilocks/ci/pre-test-script.sh
+++ stable/goldilocks/ci/pre-test-script.sh
@@ -2,4 +2,5 @@
 
 # This will label the first ndoe that is returned by kubectl get nodes so that we can test
 # the topologySpreadConstraints with `topologyKey: topology.kubernetes.io/zone`
-kubectl label $(kubectl get nodes -o name | head -n 1) topology.kubernetes.io/zone=test-zone-a
+FIRST_NODE="$(kubectl get nodes -o name | head -n 1)"
+kubectl label "$FIRST_NODE" topology.kubernetes.io/zone=test-zone-a
