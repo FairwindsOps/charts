d81b5e8be9447c8af89aa99e75bf39761f1d8087
Vitor Vezani <vitor.vezani@fairwinds.com>
2025-12-01 17:22:53 -0300

    try fix ct sf

--- scripts/circle-pre-e2e.sh
+++ scripts/circle-pre-e2e.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure git remote and master branch are available for ct list-changed
-git remote add ro https://github.com/fairwindsops/charts || true
-git fetch ro master
+# Unshallow if needed and fetch master branch
+git fetch --unshallow origin master 2>/dev/null || git fetch origin master
 
 docker cp "$(pwd)" e2e-command-runner:/charts
