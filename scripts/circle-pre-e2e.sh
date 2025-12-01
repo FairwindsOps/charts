cd7092426d67a4280ade80c0fe698e6e9faca00b
Vitor Vezani <vitor.vezani@fairwinds.com>
2025-12-01 17:14:02 -0300

    try to fix pipeline

--- scripts/circle-pre-e2e.sh
+++ scripts/circle-pre-e2e.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Ensure git remote and master branch are available for ct list-changed
+git remote add ro https://github.com/fairwindsops/charts || true
+git fetch ro master
+
 docker cp "$(pwd)" e2e-command-runner:/charts
