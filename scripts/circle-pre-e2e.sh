46ff54ef5d170ed2993e64eb99db7be2ac4edbba
Vitor Vezani <vitor.vezani@fairwinds.com>
2025-12-02 11:10:17 -0300

    using checkout full

--- scripts/circle-pre-e2e.sh
+++ scripts/circle-pre-e2e.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-# Ensure git remote and master branch are available for ct list-changed
-# Unshallow if needed and fetch master branch
-git fetch --unshallow origin master 2>/dev/null || git fetch origin master
-
 docker cp "$(pwd)" e2e-command-runner:/charts
